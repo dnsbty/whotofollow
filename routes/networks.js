6ba7300767f93fe87efe2211de71baefe2a556c5
Dennis Beatty <dennis@dcbeatty.com>
2016-03-26 16:26:29 -0600

    get all networks endpoint

--- routes/networks.js
+++ routes/networks.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Network = mongoose.model('Network');
 
 /* GET networks listing. */
 router.get('/', function(req, res, next) {
-	res.send('this will be a list of networks');
+	Network.find({}, function(err, networks) {
+		if (err)
+			next(err);
+
+		res.json(networks);
+	});
 });
 
 module.exports = router;
